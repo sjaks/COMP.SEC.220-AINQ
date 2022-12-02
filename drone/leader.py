f0f00cde45da32af34a8aaf131cef941987eb8f6
Sami Jakonen <sami.jakonen@iki.fi>
2022-12-02 15:21:54 +0200

    Start implementing gen_group_key

--- drone/leader.py
+++ drone/leader.py
@@ -1,4 +1,25 @@
 from drone.base import BaseDrone
+from random import randint
 
 class LeaderDrone(BaseDrone):
-    pass
+    K_g: int
+    l_k: int
+
+    def gen_group_key(self, GL, t_g, q):
+        C_i = []
+        self.K_g = randint(0, q)
+        self.l_k = randint(0, q)
+
+        P = 0
+        V = self.l_k * P # TODO: what is P?
+
+        cipher_texts = []
+        for drone in GL:
+            pk_i = (drone.R_i, drone.P_i) # TODO: add R_i to base.py
+
+            Y_i = "" # TODO: calculate Y_i, T_i and C_i
+            T_i = ""
+            C_i = ""
+            cipher_texts.append(C_i)
+
+        return (V, cipher_texts, t_g)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from drone.edge import EdgeDrone
 
 kgc = Kgc()
 
+# Init the KGC and the leader drone
 omega = kgc.setup()
 leader_drone = LeaderDrone()
 edge_drones = []
@@ -25,5 +26,7 @@ R_i, s_i = kgc.gen_partial_key(leader_drone.d_i, leader_drone.P_i, omega[5])
 leader_drone.full_key_gen(s_i, R_i)
 for drone in edge_drones:
     R_i, s_i = kgc.gen_partial_key(drone.d_i, drone.P_i, omega[5])
-    (pk_i, sk_i) = drone.full_key_gen(s_i, R_i) # compute the full private and public keys for each drone
-    print((pk_i, sk_i))
+    (pk_i, sk_i) = drone.full_key_gen(s_i, R_i) # compute the full keys after getting the partials
+
+# Compute a group key at the leader drone
+leader_drone.gen_group_key(edge_drones, "", kgc.q)
